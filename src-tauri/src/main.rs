c0d20d0c8f038b48ef798ff46189d7183b634914
Sebantonio <sebantonio@gmail.com>
2026-07-25 11:30:49 +0200

    build: crear estructura tauri base para android apk

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,9 @@
+// Tauri backend for Gestor Notas FP
+
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
